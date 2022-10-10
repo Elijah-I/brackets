07dba9b1b4951fb1954443b9ca6210e36f4cbc25
Elijah <elijah.ivanik@inbox.ru>
2022-10-10 18:46:30 +0300

    refactor: use stack to solve problem

--- src/index.js
+++ src/index.js
@@ -1,41 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // local var
-  let coincidence = 0
-  let length = str.length
-  const special_symbols = [['.', '+'], ['^', '$'], ['*', '?']]
+  const uid = () => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    const charactersLength = characters.length
 
-  // replace same character to different
-  const replaceSame = (replace_char, found) => {
-    const [char_open, char_close] = special_symbols[coincidence]
+    return characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  const stack = []
+  const breckets = Array.from(str)
+  const [OPEN_BRECKET, CLOSE_BRECKET_PAIR] = bracketsConfig.reduce(
+    (acc, pair) => {
+      let open = pair[0]
+      let close = pair[1]
 
-    str = Array.from(str).map(char => {
-      if (char == replace_char) {
-        return ++found % 2 ? char_open : char_close
+      if (open === close) {
+        const new_open = uid()
+        const new_close = uid()
+
+        let index = 0
+        for (const order in breckets) {
+          if (breckets[order] === open) {
+            breckets[order] = index % 2 ? new_close : new_open
+            index++
+          }
+        }
+
+        open = new_open
+        close = new_close
       }
-      return char
-    }).join('')
 
-    return [char_open, char_close, str]
+      acc[0].push(open)
+      acc[1][close] = open
+
+      return acc
+    },
+    [[], {}]
+  )
+
+  for (const brecket of breckets) {
+    let push = false
+    if (OPEN_BRECKET.includes(brecket)) push = true
+    else if (stack[stack.length - 1] !== CLOSE_BRECKET_PAIR[brecket]) push = true
+
+    if (push) stack.push(brecket)
+    else stack.pop()
   }
 
-  // create parse rules regexps
-  const parseRules = bracketsConfig
-    .map(([open, close]) => {
-      if (open == close) {
-        [open, close] = replaceSame(open, 0)
-        coincidence++
-      }
-
-      return `${open}${close}`
-    })
-
-  // kill pair-breckets untill nothing to kill
-  do {
-    length = str.length
-    for (let rule of parseRules)
-      str = str.replace(rule, '')
-  } while (str.length != length)
-
-  // if nothing left - bracket string was correct
-  return str === ''
+  return !stack.length
 }
